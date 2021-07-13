af270cbaaa6e19a4c5f5e918263af264c8a08f43
Charvisaxena01 <74494231+Charvisaxena01@users.noreply.github.com>
2021-07-13 20:05:35 +0530

    Update food.js

--- js/food.js
+++ js/food.js
@@ -36,8 +36,17 @@ class Food {
       var x=80,y=100;
       
       imageMode(CENTER);
-      image(this.image,620,320,70,70);
-      
+  
+         fill(255,255,254)
+   textSize(15)
+   if(lastFed>=12){
+     text("LastFeed :" + lastFed%12+"PM", 350,30)
+   }else if(lastFed == 0){
+     text("Last Feed : 12 AM", 350,30)
+
+   }else{
+     text("Last Feed : " + lastFed+"AM",350,30)
+   }
       if(this.foodStock!=0){
         for(var i=0;i<this.foodStock;i++){
           if(i%10==0){
@@ -49,4 +58,5 @@ class Food {
         }
       }
     }
+    
 }
